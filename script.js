749532eddc912ffbce4e19b28f2e29536c40e6ad
Taii <126676872+oliveiratainara948@users.noreply.github.com>
2023-06-14 12:12:45 -0300

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let Fruta = "";
+
+switch(Fruta) {
+    case "maçã":
+        alert("Não vendemos esta fruta aqui")
+        break;
+    
+    case "kiwi":
+        alert("Estamos com escassez de kiwis")
+        break;
+
+    case "melancia":
+        alert("Aqui está, são 3 reais o quilo")
+        break;
+    
+    default:
+        alert("Desculpe não temos mais opções de frutas")
+        break;
+}
